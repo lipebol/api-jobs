70b8de20715785ed6c7443971050ddf46a67b707
lipebol <lipe_bol5@hotmail.com>
2024-05-24 22:11:22 -0300

    add model

---
+++ src/main/java/com/api/jobs/models/JobsModel.java
@@ -0,0 +1,43 @@
+package com.api.jobs.models;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "TB_tests")
+public class JobsModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public BigDecimal getValue() {
+        return value;
+    }
+    
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
